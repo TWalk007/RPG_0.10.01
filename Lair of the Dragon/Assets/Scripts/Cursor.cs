f5fae881f4b4f1a95a352f8c82ab3c95bb307781
Tom Walker <walkertb03@gmail.com>
2017-07-20 18:44:20 -0400

    Basic Scene setup with Raycast Hits Working

---
+++ Lair of the Dragon/Assets/Scripts/Cursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster cameraRayCaster;
+
+	void Start () {
+        cameraRayCaster = GetComponent<CameraRaycaster>();
+	}
+
+	void Update () {
+		print (cameraRayCaster.layerHit);
+	}
+}

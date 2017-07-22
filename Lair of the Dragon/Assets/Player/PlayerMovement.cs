5343c11a9a92901761037d83e45b7a2478619593
Tom Walker <walkertb03@gmail.com>
2017-07-22 14:43:59 -0400

    Controller Movement Add

--- Lair of the Dragon/Assets/Player/PlayerMovement.cs
+++ Lair of the Dragon/Assets/Player/PlayerMovement.cs
@@ -10,7 +10,9 @@ public class PlayerMovement : MonoBehaviour
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+    bool isInDirectMode = false; //TODO consider making static.
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -20,6 +22,39 @@ public class PlayerMovement : MonoBehaviour
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
+    {
+        // G for gamepad.  TODO allow player to map later.
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            //toggle mode.
+            isInDirectMode = !isInDirectMode;
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+
+        ProcessMouseMovement();
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement()
     {
         if (Input.GetMouseButton(0))
         {
@@ -48,6 +83,5 @@ public class PlayerMovement : MonoBehaviour
             m_Character.Move(Vector3.zero, false, false);
         }
     }
-
 }
 
